fe7df56b20bb1c020a3c43942d5e3ccbc211ef01
Matt Cameron <cameron.matt@gmail.com>
2018-12-15 20:58:20 -0600

    amplify auth

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify from 'aws-amplify';
+import aws_exports from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+Amplify.configure(aws_exports);
+
 class App extends Component {
   render() {
     return (
@@ -25,4 +30,16 @@ class App extends Component {
   }
 }
 
-export default App;
+const signUpConfig = {
+  signUpFields: [
+    {
+      label: 'Organization',
+      key: "organization",
+      required: false,
+      type: "string",
+      custom: "true"
+    },
+  ],
+};
+
+export default withAuthenticator(App, { includeGreetings: true, signUpConfig });
